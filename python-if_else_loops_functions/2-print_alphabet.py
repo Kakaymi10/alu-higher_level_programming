cb64e2b8a6a8a66b47e3abed0a157d82cba48a2c
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2022-10-22 11:53:49 +0200

    Update 2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""import string
-print(string.ascii_lowercase)
+#import string
+#print(string.ascii_lowercase)
 
-import string
-v = string.ascii_lowercase
-print('{}'.format(v))"""
+#import string
+#v = string.ascii_lowercase
+#print('{}'.format(v))"""
 
 for c in range(ord('a'), ord('z')+1):
     print('{}'.format(chr(c)), end='')
