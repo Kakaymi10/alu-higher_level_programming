e273def84400fb2131dee61a916f437535875125
Kakaymi10 <moussa@alustudent.com>
2022-10-22 02:33:18 -0700

    alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""import string
+print(string.ascii_lowercase)"""
+
 import string
-print(string.ascii_lowercase)
+v = string.ascii_lowercase
+print('{}'.format(v))
+
+"""for c in range(ord('a'), ord('z')+1):
+    print('{}'.format(chr(c)), end='')"""
