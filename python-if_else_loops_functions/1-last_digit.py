48fb63fe6423b6d1e64ca5f91f74ab6be99193f4
Kakaymi10 <moussa@alustudent.com>
2022-10-22 01:47:35 -0700

    last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -13,9 +13,9 @@ if number < 0:
 
 if int(last) > 5:
     number1 = " and is greater than 5"
-if int(last) == 0:
+elif int(last) == 0:
     number1 = " and is 0"
-if int(last) < 0:
+else:
     number1 = " and is less than 6 and not 0"
 
 
