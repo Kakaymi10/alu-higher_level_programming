dfff31252bd9de7ac573ef4619826db58ac1faa3
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2022-10-22 11:59:20 +0200

    Update 2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z')+1):
     print('{}'.format(chr(c)), end='')
+    
+    
+#import string
+#print(string.ascii_lowercase)
+
+#import string
+#v = string.ascii_lowercase
+#print('{}'.format(v))"""
+    
