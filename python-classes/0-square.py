5c9d0a7908cc75f0b6c9a31d686cdd88e55b3dcb
Kakaymi10 <moussa@alustudent.com>
2022-11-09 21:32:22 -0800

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module 0-squre
-Defines class Square"""
+"""Module 0-squre and defines class Square."""
+
+
 
 class Square():
-"""Empty class"""
+"""Empty class."""
     pass
