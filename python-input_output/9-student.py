b40dc70cb4609fd313b4bcfd831131602d0e551a
Kakaymi10 <moussa@alustudent.com>
2022-11-17 09:45:09 -0800

    Student A.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -21,7 +21,7 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        def to_json(self):
+     def to_json(self):
         """
         Returns dictionary description with simple data structure
         (list, dictionary, dictionary, string)
