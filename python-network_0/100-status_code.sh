a016af8cfaa7ef4f162a2913a740f61be8154d23
Kakaymi10 <m.moussa@alustudent.com>
2022-11-18 19:58:39 +0000

    Network 06

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that sends a request to a url passed as an argument,and displays only the status code of the response
-curl -so /dev/null --write-out "%{http_code}" "$1"
+# Take in URL, display status code only; Usage: ./100-status_code.sh 0.0.0.0:5000/nop ; echo ""
+curl -o /dev/null -w '%{http_code}' -sLI "$1"
