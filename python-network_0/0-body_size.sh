e77a84d6262985182b8818df0a122f602175a577
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2022-11-18 21:24:33 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes URL and displays the size of the response body
-curl -sI "$1" | grep -i 'Content-Length' | cut -d " " -f2
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
