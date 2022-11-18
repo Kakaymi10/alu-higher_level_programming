856575842eb5766f873c76e5373dd55e62109884
Kakaymi10 <m.moussa@alustudent.com>
2022-11-18 19:36:32 +0000

    Network 00

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 # Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
-=======
-#!/bin/bash
-# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
->>>>>>> 74b62a9dd10223d5d9dedc1ae1f494359bb41b9c
