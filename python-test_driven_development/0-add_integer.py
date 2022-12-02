0797f75375790db56bd630933e84e7031ad665d8
Kakaymi10 <m.moussa@alustudent.com>
2022-12-01 23:37:00 -0800

    Addi integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-""" Defining the function """
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
+
+
 def add_integer(a, b=98):
     """Returns a + b as in"""
     if not isinstance(a, (int, float)):
