37638474b37b36d82a7ee1d5a7af30e8384d107e
Kakaymi10 <m.moussa@alustudent.com>
2022-12-05 03:56:38 -0800

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,11 +4,10 @@
 def text_indentation(text):
     "Raise exceptions"
     if not isinstance(text, str):
-        raise TypeError("text must be an string")
+        raise TypeError("text must be a string")
     
     for char in ".?:":
         text = text.replace(char, char + "\n\n")
     list_lines = [lines.strip(' ') for lines in text.split('\n')]
     revised = "\n".join(list_lines)
     print(revised, end="")
-
