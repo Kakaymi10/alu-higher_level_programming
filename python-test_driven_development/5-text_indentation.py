112a81c19d82f546a09bc66fb00667d7fc09d4e4
Kakaymi10 <m.moussa@alustudent.com>
2022-12-05 03:06:12 -0800

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"Write a function that prints a text with 3=2 new lines after each of these characyters:., ?, and :"""
+
+def text_indentation(text):
+    "Raise exceptions"
+    if not isinstance(text, str):
+        raise TypeError("text must be an string")
+    
+    for char in ".?:":
+        text = text.replace(char, char + "\n\n")
+    list_lines = [lines.strip(' ') for lines in text.split('\n')]
+    revised = "\n".join(list_lines)
+    print(revised, end="")
+
