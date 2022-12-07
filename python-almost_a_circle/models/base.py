dc1e8ea58cc3806b983b7e6a7a7e83021ef7ff92
Kakaymi10 <m.moussa@alustudent.com>
2022-12-07 01:38:36 -0800

    Hmm

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Creating class"""
+
+
 class Base:
     """private class attributes"""
     __nb_objects = 0
+
     def __init__(self, id = None):
         if id != None:
             self.id = id
