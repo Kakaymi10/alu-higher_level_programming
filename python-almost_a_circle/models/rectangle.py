ecd6af477a66239f7d76b12c53f8ff258d510726
Kakaymi10 <m.moussa@alustudent.com>
2022-12-07 05:29:25 -0800

    Craziness

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,8 +5,7 @@ Inherits from Base;
 Inits superclass' id
 Contains private width, height, x, y
 """
-import base
-Base = base.Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -20,3 +19,43 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self .y = y
+
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """setter width"""
+        self.__width = value
+    
+    @height.setter
+    def height(self,value):
+        """setter height"""
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """setter x"""
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """setter y"""
+        self.__y = value
