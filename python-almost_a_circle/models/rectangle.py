37bd2112f4af21b72c8fe48777904c338946e354
Kakaymi10 <m.moussa@alustudent.com>
2022-12-07 07:00:27 -0800

    shiit(

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,57 +6,52 @@ Inits superclass' id
 Contains private width, height"""
 
 
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    defines class Rectangle; inherits from class Base
-    """
+    '''A Rectangle class.'''
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize"""
+        '''Constructor.'''
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
-        self .y = y
+        self.y = y
 
     @property
     def width(self):
-        """getter width"""
+        '''Width of this rectangle.'''
         return self.__width
-
-    @property
-    def height(self):
-        """getter height"""
-        return self.__height
-
-    @property
-    def x(self):
-        """getter x"""
-        return self.__x
-
-    @property
-    def y(self):
-        """getter y"""
-        return self.__y
 
     @width.setter
     def width(self, value):
-        """setter width"""
         self.__width = value
-    
+
+    @property
+    def height(self):
+        '''Height of this rectangle.'''
+        return self.__height
+
     @height.setter
-    def height(self,value):
-        """setter height"""
+    def height(self, value):
         self.__height = value
+
+    @property
+    def x(self):
+        '''x of this rectangle.'''
+        return self.__x
 
     @x.setter
     def x(self, value):
-        """setter x"""
         self.__x = value
+
+    @property
+    def y(self):
+        '''y of this rectangle.'''
+        return self.__y
 
     @y.setter
     def y(self, value):
-        """setter y"""
         self.__y = value
