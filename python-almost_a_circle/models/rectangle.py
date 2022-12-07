2684326c4ef2b7a676dfda8942a880488bcbb18f
Kakaymi10 <m.moussa@alustudent.com>
2022-12-07 04:28:18 -0800

    rrrr

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,26 +5,13 @@ Inherits from Base;
 Inits superclass' id
 Contains private width, height, x, y
 """
-from base import Base
+import base
+Base = base.Base
 
 
 class Rectangle(Base):
     """
     defines class Rectangle; inherits from class Base
-    Inherited Attributes:
-        id
-    Class Attributes:
-        __width          __height
-        __x              __y
-    Methods:
-        __init__(self, width, height, x=0, y=0, id=None):
-        update(self, *args, **kwargs)
-        width(self)      width(self, value)
-        height(self)     height(self, value)
-        x(self)          x(self, value)
-        y(self)          y(self, value)
-        area(self)       display(self)
-        __str__          to_dictionary(self)
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize"""
