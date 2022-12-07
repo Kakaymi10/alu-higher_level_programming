835068627a380a604bcc95e3ddff138fd45755c9
Kakaymi10 <m.moussa@alustudent.com>
2022-12-07 02:54:12 -0800

    Almost

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,22 +8,22 @@ Base = base.Base
 
 
 """impoerting Pep package for pep validation"""
-
-
 import pep8
 
 
 """Classes for pep8 validation for models/base.py and tests/test_models/test_base.py"""
-
-
-class Pep8Test(unittest.TestCase):
+class TestPep8(unittest.TestCase):
+    """Pep8 models/base.py & tests/test_models/test_base.py"""
     def test_pep8(self):
+        """Pep8"""
+        style = pep8.StyleGuide(quiet=False)
+        errors = 0
+        files = ["models/base.py", "tests/test_models/test_base.py"]
+        errors += style.check_files(files).total_errors
+        self.assertEqual(errors, 0, 'Need to fix Pep8')
         
 
-
 """Test class for models/bae file"""
-
-
 class TestBase(unittest.TestCase):
 
     """call set up at the beginning of test"""
@@ -36,6 +36,10 @@ class TestBase(unittest.TestCase):
 
     """Test functions for the 2nd task"""
     def test_base_1_id(self):
+        self.assertTrue(Base(89), self.id == 89)
+        self.assertTrue(Base(0), self.id == 0)
+        self.assertTrue(Base(), self.id == 1)
+        self.assertTrue(Base(), self.id == 2)
         
 
 
