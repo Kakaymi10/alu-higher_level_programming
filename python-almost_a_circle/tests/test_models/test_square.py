949dc37f1409637c895b12d7264445293bef6f11
Kakaymi10 <m.moussa@alustudent.com>
2022-12-13 10:14:34 -0800

    Tests

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,127 +1,191 @@
 #!/usr/bin/python3
-"""
-Unittest for Square Class
-# run with python3 -m unittest discover tests
-# run with python3 -m unittest tests/test_models/test_square.py
-"""
+"""Test for Square"""
 
-
+import os
 import unittest
-import pep8
 from io import StringIO
-from contextlib import redirect_stdout
-from models import square
-Square = square.Square
+from unittest.mock import patch
+
+from models.base import Base
+from models.square import Square
 
 
-class TestPep8(unittest.TestCase):
-    """Pep8 models/square.py & tests/test_models/test_square.py"""
-    def test_pep8(self):
-        """Pep8"""
-        style = pep8.StyleGuide(quiet=False)
-        errors = 0
-        files = ["models/square.py", "tests/test_models/test_square.py"]
-        errors += style.check_files(files).total_errors
-        self.assertEqual(errors, 0, 'Need to fix Pep8')
+class TestSquare(unittest.TestCase):
+    """Test for class Square"""
 
+    def test_instance(self):
+        """Doc"""
+        s = Square(1)
+        s1 = Square(1, 2)
+        s2 = Square(1, 2, 3)
+        s12 = Square(1, 0)
+        s0 = Square(1, 2, 3, 4)
 
-class TestBase(unittest.TestCase):
-    """Tests for models/square.py"""
+        self.assertEqual(s0.id, 4)
 
-    """Test attributes"""
-    def test_all_attr_given(self):
-        """Test all attributes match what's given"""
-        s1 = Square(9, 99, 999, 1000)
-        self.assertTrue(s1.width == 9)
-        self.assertTrue(s1.height == 9)
-        self.assertTrue(s1.size == 9)
-        self.assertTrue(s1.x == 99)
-        self.assertTrue(s1.y == 999)
-        self.assertTrue(s1.id == 1000)
-
-    def test_default_attr(self):
-        """Test default attributes are set when not given"""
-        s2 = Square(88)
-        self.assertTrue(s2.width == 88)
-        self.assertTrue(s2.height == 88)
-        self.assertTrue(s2.size == 88)
-        self.assertTrue(s2.x == 0)
-        self.assertTrue(s2.y == 0)
-        self.assertTrue(s2.id is not None)
-
-    def test_attr_validated(self):
-        """Test attributes are validated before set"""
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("10")
-            Square([10, 3])
-            Square({20, })
-            Square({"d": 20})
-            Square(None)
-            Square((30, 20), 4)
         with self.assertRaisesRegex(ValueError, "width must be > 0"):
-            Square(-1)
-            Square(9).size(-9)
+            s9 = Square(-1, 2)
 
-    """Test args given"""
-    def test_invalid_args(self):
-        """Test too many args given throws error"""
-        with self.assertRaises(TypeError):
-            Square(1, 2, 3, 4, 5, 6, 7)
-        """Test too little args given throws error"""
-        with self.assertRaises(TypeError):
-            Square()
-            Square(None)
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            s10 = Square(1, -2)
 
-    """Test class"""
-    def test_class(self):
-        """Test class created is indeed Rectangle"""
-        s = Square(10)
-        self.assertEqual(type(s), Square)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            s11 = Square(0, 2)
 
-    """Test methods"""
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            s13 = Square(1, 2, -3)
+
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            s = Square(0)
+
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            s4 = Square("1")
+
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            s5 = Square(1, "2")
+
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            s6 = Square(1, 2, "3")
+
     def test_area(self):
-        """Test method: area"""
-        self.assertEqual(Square(3).area(), 9)
-        self.assertEqual(Square(4, 0, 0).area(), 16)
+        """Doc"""
+        s1 = Square(2)
+        self.assertEqual(s1.area(), 4)
+
+    def test__str__(self):
+        """Doc"""
+        Base._Base__nb_objects = 0
+        s1 = Square(2)
+        with patch("sys.stdout", new=StringIO()) as fake_out:
+            print(s1)
+            self.assertEqual(fake_out.getvalue(),
+                             "[Square] (1) 0/0 - 2\n")
 
     def test_display(self):
-        """Test method: display"""
-        with StringIO() as bufr, redirect_stdout(bufr):
-            Square(4).display()
-            b = bufr.getvalue()
-        self.assertEqual(b, '####\n####\n####\n####\n')
-        with StringIO() as bufr, redirect_stdout(bufr):
-            Square(3, 1, 2).display()
-            b = bufr.getvalue()
-        self.assertEqual(b, '\n\n ###\n ###\n ###\n')
-
-    def test_print(self):
-        """Test method: __str__"""
-        s = Square(1, 2, 3, 44)
-        s.size = 500
-        self.assertEqual(str(s), '[Square] (44) 2/3 - 500')
-
-    def test_update(self):
-        """Test method: update(*args)"""
-        s = Square(1, 2, 3, 4)
-        s.update(10, 10, 10, 10)
-        self.assertEqual(str(s), '[Square] (10) 10/10 - 10')
-        s.update()
-        self.assertEqual(str(s), '[Square] (10) 10/10 - 10')
-        s.update(99)
-        self.assertEqual(str(s), '[Square] (99) 10/10 - 10')
-        s.update(99, 5)
-        self.assertEqual(str(s), '[Square] (99) 10/10 - 5')
-        s.update(44, 55, 1, 2)
-        self.assertEqual(str(s), '[Square] (44) 1/2 - 55')
-        """Test method: update(*kwargs)"""
-        s.update(id=88, size=77, nokey=99)
-        self.assertEqual(str(s), '[Square] (88) 1/2 - 77')
+        """Doc"""
+        s1 = Square(2)
+        s2 = Square(2, 2, 3)
+        with patch("sys.stdout", new=StringIO()) as fake_out:
+            s1.display()
+            self.assertEqual(fake_out.getvalue(),
+                             "##\n##\n")
+        with patch("sys.stdout", new=StringIO()) as fake_out:
+            s2.display()
+            self.assertEqual(fake_out.getvalue(),
+                             "\n\n\n  ##\n  ##\n")
 
     def test_to_dictionary(self):
-        """Test method: to_dictionary"""
-        sdic = Square(1, 2, 3, 4).to_dictionary()
-        self.assertEqual(type(sdic), dict)
-        s2 = Square(10, 10)
-        s2.update(**sdic)
-        self.assertEqual(str(s2), '[Square] (4) 2/3 - 1')
+        """Doc"""
+        Base._Base__nb_objects = 0
+        s1 = Square(4)
+        self.assertEqual(s1.to_dictionary(),
+                         {'id': 1, 'size': 4, 'x': 0, 'y': 0})
+
+    def test_update(self):
+        """Doc"""
+        Base._Base__nb_objects = 0
+        s1 = Square(2)
+        s1.update()
+        self.assertEqual(s1.id, 1)
+
+        s1.update(89)
+        self.assertEqual(s1.id, 89)
+
+        s1.update(89, 1)
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+
+        s1.update(89, 1, 2)
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+
+        s1.update(89, 1, 2, 3)
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+        self.assertEqual(s1.y, 3)
+
+        s1.update(**{'id': 89})
+        self.assertEqual(s1.id, 89)
+
+        s1.update(**{'id': 89, 'size': 1})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+
+        s1.update(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+
+        s1.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+        self.assertEqual(s1.y, 3)
+
+    def test_create(self):
+        """Doc"""
+
+        s1 = Square.create(**{'id': 89})
+        self.assertEqual(s1.id, 89)
+
+        s1 = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+
+        s1 = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+
+        s1 = Square.create(**{'id': 89, 'size': 1,
+                              'x': 2, 'y': 3})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+        self.assertEqual(s1.y, 3)
+
+        s1 = Square.create(**{'id': 89, 'size': 1,
+                              'x': 2, 'y': 3})
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.x, 2)
+        self.assertEqual(s1.y, 3)
+
+    def test_save_to_file(self):
+        """Doc"""
+        Base._Base__nb_objects = 0
+
+        Square.save_to_file(None)
+        self.assertTrue(os.path.isfile("Square.json"))
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), '[]')
+
+        Square.save_to_file([])
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), '[]')
+            self.assertEqual(type(file.read()), str)
+
+        Square.save_to_file([Square(1)])
+        with open("Square.json") as file:
+            self.assertEqual(file.read(),
+                             '[{"id": 1, "size": 1, "x": 0, "y": 0}]')
+
+    def test_save_to_file_empty(self):
+        Square.save_to_file([])
+        self.assertTrue(os.path.isfile("Square.json"))
+        with open("Square.json") as file:
+            self.assertEqual(file.read(), "[]")
+            self.assertEqual(type(file.read()), str)
+
+    def test_load_from_file(self):
+        """Doc"""
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
+
+        self.assertEqual(Square.load_from_file(), [])
+        Square.save_to_file([Square(2)])
+        from_file = Square.load_from_file()
+        self.assertEqual(type(from_file), list)
+        self.assertEqual(from_file[0].size, 2)
