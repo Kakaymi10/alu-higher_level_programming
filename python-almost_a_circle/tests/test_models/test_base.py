c6d28426f3412a3f6b329b7e97a26c9a61c4bcbc
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2022-12-15 16:59:10 +0200

    Update test_base.py

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-"""Test cases for Base"""
+"""
+    Unittest for Base
+"""
 
-import os
 import unittest
+from models import base
+Base = base.Base
 
-from models.base import Base
 
 
-class Test_Base(unittest.TestCase):
-    """Test class for Base"""
+class TestBase(unittest.TestCase):
+    """
+        test for Base
+    """
+    def test_creation_id(self):
+        """
+            test if value of id has the good assignment
+        """
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base(-5)
+        b6 = Base(6.3)
+        b7 = Base()
+        b8 = Base(None)
 
-    def test_basic(self):
-        """Doc"""
-        self.assertEqual(Base(), 1)
-        self.assertEqual(Base(), 2)
-        self.assertEqual(Base(89), 89)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, -5)
+        self.assertEqual(b6.id, 6.3)
+        self.assertEqual(b7.id, 4)
+        self.assertEqual(b8.id, 5)
+
+        
+if __name__ == '__main__':
+    unittest.main()
