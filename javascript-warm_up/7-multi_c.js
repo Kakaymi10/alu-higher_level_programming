45edacf603ebd7c75c94c8c1887a792a8c5be33f
Kakaymi1à <m.moussa@alustudent.com>
2023-01-30 10:37:24 -0800

    I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
-function multiC() {
+function multiC () {
   if (parseInt(process.argv[2])) {
     for (let i = 0; i < parseInt(process.argv[2]); i++) {
-     console.log('C is fun');
+      if (parseInt(process.argv[2]) <= 0) {
+        break;
+      } else {
+        console.log('C is fun');
+      }
     }
-  } else if (parseInt(process.argv[2]) <= 0) {
-    pass;
   } else {
     console.log('Missing number of occurrences');
   }
 }
-multiC()
+multiC.call();
