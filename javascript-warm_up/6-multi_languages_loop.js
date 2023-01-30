89f31dcf63b9776c6d4b2323e1ca5f8250cd9619
Kakaymi1à <m.moussa@alustudent.com>
2023-01-30 09:13:35 -0800

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function loopToLanguages() {
-  let n =  0;
+function loopToLanguages () {
+  let n = 0;
   while (n < 1) {
     console.log(`C is fun
 Python is cool
-Javascript is amazing`);
+JavaScript is amazing`);
     n++;
   }
 }
